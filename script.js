4441f4a21673b84ca32afd6bda27bd19aed3fe07
Evan Earle <evan.earle@gmail.com>
2023-05-13 11:02:37 -0300

    new origin

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("testing");
+let screen = document.getElementById("result");
+
+document.querySelectorAll(".button").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let input = button.innerHTML;
+    userClicked(input);
+  });
+});
+
+const userClicked = (button) => {
+  switch (button) {
+    case "=":
+      evaluate();
+      break;
+    case ".":
+      addNumber(".");
+      break;
+    case "/":
+      addNumber("/");
+      break;
+    case "X":
+      addNumber("*");
+      break;
+    case "+":
+      addNumber("+");
+      break;
+    case "-":
+      addNumber("-");
+      break;
+    default:
+      addNumber(button);
+  }
+};
+
+const addNumber = (num) => {
+  screen.innerText = (screen.innerText + num).substring(0, 9);
+};
+
+const evaluate = () => {
+  let formula = screen.innerHTML;
+  screen.innerText = eval(formula);
+};
